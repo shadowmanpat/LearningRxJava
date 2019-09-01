0cdbec399f90d257be75b70724104e321d7497ee
nagas <nagas@myjobnow.com>
2019-09-01 11:47:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/learningrxjava/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.learningrxjava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/learningrxjava/SimpleExample/SimpleRx.kt
@@ -0,0 +1,5 @@
+package com.example.learningrxjava.SimpleExample
+
+object SimpleRx {
+
+}

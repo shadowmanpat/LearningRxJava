5482d223883e1a602ed1f51538a6308a4765475e
nagas <nagas@myjobnow.com>
2019-09-01 11:56:47 +0300

    behavior relays

--- app/src/main/java/com/example/learningrxjava/SimpleExample/SimpleRx.kt
+++ app/src/main/java/com/example/learningrxjava/SimpleExample/SimpleRx.kt
@@ -1,5 +1,25 @@
 package com.example.learningrxjava.SimpleExample
 
+import com.jakewharton.rxrelay2.BehaviorRelay
+import io.reactivex.disposables.CompositeDisposable
+
 object SimpleRx {
+
+    var bag = CompositeDisposable()
+
+    fun simpleValues(){
+        println("--------SimpleValues")
+
+        val someInfo = BehaviorRelay.createDefault("1")
+        print("${someInfo.value}")
+
+        someInfo.accept("2")
+
+        someInfo.subscribe(){newValue->
+            println( "newValue $newValue")
+        }
+
+        someInfo.accept("3")
+    }
 
 }
